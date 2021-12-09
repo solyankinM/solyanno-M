fd1e4c610e748b5433f873c3eb323d54bcea91f0
matvey <solyanov.mat@mail.ru>
2021-12-09 21:44:39 +0300

    database

---
+++ SQL.sql
@@ -0,0 +1,8 @@
+﻿SELECT
+  book.namebook
+FROM book
+  INNER JOIN autor
+    ON book.id_autor = autor.id_autor
+  INNER JOIN janr
+    ON book.id_janr = janr.id_janr
+WHERE autor.firstname = 'Пушкин'
